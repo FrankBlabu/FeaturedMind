1930bb55b64cc51eacf4d6931699e57ab4e40faa
Frank Blankenburg <frankcieslok@gmail.com>
2017-02-25 16:24:11 +0100

    Rectangle feature generation implemented.

--- generate_test_data_set.py
+++ generate_test_data_set.py
@@ -42,6 +42,7 @@ class Configuration:
         self.number_of_images = args.number_of_images
         self.sample_size      = args.sample_size
 
+
 #--------------------------------------------------------------------------
 # Create an elliptical feature
 #
@@ -97,6 +98,19 @@ def get_segment (rect, segment):
              rect[0][1] + int (round (y * size[1] / 3))),
             (rect[0][0] + int (round ((x + 1) * size[0] / 3)),
              rect[0][1] + int (round ((y + 1) * size[1] / 3)))]
+
+#--------------------------------------------------------------------------
+# Expand the segment to the bounding box of two segments
+#
+# @param rect1 Segment rect 1 [(x0, y0), (x1, y1)]
+# @param rect2 Segment rect 2 [(x0, y0), (x1, y1)]
+# @return Resulting segment covering the bounding box
+#
+def expand_segment (rect1, rect2):
+    return [(min (rect1[0][0], rect2[0][0]), min (rect1[0][1], rect2[0][1])),
+            (max (rect1[1][0], rect2[1][0]), max (rect1[1][1], rect2[1][1]))]
+    
+    
 #--------------------------------------------------------------------------
 # Return corner point of rectangle
 #
@@ -116,6 +130,56 @@ def get_rect_point (rect, n):
 
     assert False and 'Illegal rectangle point index'
 
+#--------------------------------------------------------------------------
+# Return size of a rectangle
+#
+# @param rect Rectangle
+# @return Size of the rectangle
+def get_rect_size (rect):
+    return (rect[1][0] - rect[0][0] + 1, rect[1][1] - rect[0][1] + 1)
+
+
+#--------------------------------------------------------------------------
+# Generate random feature
+#
+# @param config Configuration
+# @param size   Size of the area the feature may occupy
+def create_feature (config, size):
+    feature_image = PIL.Image.new ('RGBA', size)
+    draw = PIL.ImageDraw.Draw (feature_image)
+
+    inner_offset = (int (.05 * size[0]), int (.05 * size[1]))
+    inner_size = (size[0] - 2 * inner_offset[0], size[1] - 2 * inner_offset[1])
+
+    assert inner_size[0] > 10 and inner_size[1] > 10
+
+    feature_size = (random.randint (10, inner_size[0]),
+                    random.randint (10, inner_size[1]))
+    
+    feature_offset = (inner_offset[0] + random.randint (0, inner_size[0] - feature_size[0]),
+                      inner_offset[1] + random.randint (0, inner_size[1] - feature_size[1]))
+
+    feature_type = 0 # random.randint (0, 2)
+    
+    #
+    # Feature type 1: Rectangle
+    #
+    if feature_type == 0:
+        draw.rectangle ((feature_offset, feature_size), fill=None, outline='#ffffff')
+        
+    #
+    # Feature type 2: Circle
+    #
+    elif feature_type == 1:
+        pass        
+    
+    #
+    # Feature type 3: Slotted hole
+    #
+    elif feature_type == 2:
+        pass
+
+    return feature_image
 
 #--------------------------------------------------------------------------
 # Generate random training image
@@ -129,14 +193,7 @@ def generate_training_image (config):
     # Final image
     #
     image = PIL.Image.new ('RGBA', (config.width, config.height))
-    #draw = PIL.ImageDraw.Draw (image)
-
-    #
-    # Image pattern overlay
-    #
-    pattern = PIL.Image.new ('RGBA', image.size)
-    draw_pattern = PIL.ImageDraw.Draw (pattern)
-
+    
     #
     # Compute area used for the specimen border
     #
@@ -164,62 +221,62 @@ def generate_training_image (config):
                                     outer_border_limit[1][1]))]
 
     #
-    # Compute segments of the specimen used. There are 3x3 segments available
+    # Compute segments of the specimen used. There are 3x3 segments available.
     #
-    used_segments = 3 * [3 * [True]]
+    available = [[True, True, True], [True, True, True], [True, True, True]]
     border = []
 
     segment_mode = random.randint (0, 2)
 
     #
-    # Corner segments might be missing 
+    # Case 1: Corner segments might be missing 
     #
     if segment_mode == 0:
-        used_segments[0][0] = random.randint (0, 9) > 5
-        used_segments[2][0] = random.randint (0, 9) > 5
-        used_segments[0][2] = random.randint (0, 9) > 5
-        used_segments[2][2] = random.randint (0, 9) > 5
+        available[0][0] = random.randint (0, 9) > 5
+        available[2][0] = random.randint (0, 9) > 5
+        available[0][2] = random.randint (0, 9) > 5
+        available[2][2] = random.randint (0, 9) > 5
 
         segment = get_segment (border_rect, (0, 0))
-        used = used_segments[0][0]
+        used = available[0][0]
         
         border.append (get_rect_point (segment, 3))
         border.append (get_rect_point (segment, 0 if used else 2))
         border.append (get_rect_point (segment, 1))
 
         segment = get_segment (border_rect, (2, 0))
-        used = used_segments[2][0]
+        used = available[2][0]
 
         border.append (get_rect_point (segment, 0))
         border.append (get_rect_point (segment, 1 if used else 3))
         border.append (get_rect_point (segment, 2))
 
         segment = get_segment (border_rect, (2, 2))
-        used = used_segments[2][2]
+        used = available[2][2]
 
         border.append (get_rect_point (segment, 1))
         border.append (get_rect_point (segment, 2 if used else 0))
         border.append (get_rect_point (segment, 3))
 
         segment = get_segment (border_rect, (0, 2))
-        used = used_segments[0][2]
+        used = available[0][2]
 
         border.append (get_rect_point (segment, 2))
         border.append (get_rect_point (segment, 3 if used else 1))
         border.append (get_rect_point (segment, 0))
 
     #
-    # Top/down edge segments might be missing
+    # Case 2: Top/down edge segments might be missing
     #
     elif segment_mode == 1:
-        used_segments[1][0] = random.randint (0, 9) > 5
-        used_segments[1][2] = random.randint (0, 9) > 5
+        available[1][0] = random.randint (0, 9) > 5
+        available[1][2] = random.randint (0, 9) > 5
 
         segment = get_segment (border_rect, (0, 0))
         border.append (get_rect_point (segment, 0))
         
         segment = get_segment (border_rect, (1, 0))
-        used = used_segments[1][0]
+        used = available[1][0]
         
         border.append (get_rect_point (segment, 0))
         if not used:
@@ -234,7 +291,7 @@ def generate_training_image (config):
         border.append (get_rect_point (segment, 2))
 
         segment = get_segment (border_rect, (1, 2))
-        used = used_segments[1][2]
+        used = available[1][2]
         
         border.append (get_rect_point (segment, 2))
         if not used:
@@ -247,11 +304,11 @@ def generate_training_image (config):
 
 
     #
-    # Left/right edge segments might be missing
+    # Case 3: Left/right edge segments might be missing
     #
     elif segment_mode == 2:
-        used_segments[0][1] = random.randint (0, 9) > 5
-        used_segments[2][1] = random.randint (0, 9) > 5
+        available[0][1] = random.randint (0, 9) > 5
+        available[2][1] = random.randint (0, 9) > 5
 
         segment = get_segment (border_rect, (0, 0))
         border.append (get_rect_point (segment, 0))
@@ -260,7 +317,7 @@ def generate_training_image (config):
         border.append (get_rect_point (segment, 1))
         
         segment = get_segment (border_rect, (2, 1))
-        used = used_segments[2][1]
+        used = available[2][1]
 
         border.append (get_rect_point (segment, 1))
         if not used:        
@@ -275,7 +332,7 @@ def generate_training_image (config):
         border.append (get_rect_point (segment, 3))
 
         segment = get_segment (border_rect, (0, 1))
-        used = used_segments[0][1]
+        used = available[0][1]
         
         border.append (get_rect_point (segment, 3))
         if not used:
@@ -283,12 +340,39 @@ def generate_training_image (config):
             border.append (get_rect_point (segment, 1))
         border.append (get_rect_point (segment, 0))
 
-        
-    draw_pattern.rectangle (border_rect, fill=None, outline='#555555')
-    draw_pattern.polygon (border, fill=None, outline='#ffffff')
+    #
+    # Copy border overlay into main image
+    #
+    border_image = PIL.Image.new ('RGBA', image.size)
+    draw_features = PIL.ImageDraw.Draw (border_image)
 
-    image.paste (pattern, mask=pattern)
-    
+    draw_features.polygon (border, fill=None, outline='#ffffff')
+
+    image.paste (border_image, mask=border_image)
+
+    #
+    # Add some features to the available areas
+    #
+    print ("Start", available)
+    for y in range (0, 3):
+        for x in range (0, 3):
+            print (x, y, available[x][y])
+            if available[x][y] and random.randint (0, 9) < 5:
+                area = get_segment (border_rect, (x, y))
+                available[x][y] = False
+                
+                if x < 2 and available[x+1][y] and random.randint (0, 9) < 5:
+                    area = expand_segment (area, get_segment (border_rect, (x+1, y)))
+                    available[x+1][y] = False
+
+                if y < 2 and available[x][y+1] and random.randint (0, 9) < 5:
+                    area = expand_segment (area, get_segment (border_rect, (x, y+1)))
+                    available[x][y+1] = False
+            
+                feature_image = create_feature (config, get_rect_size (area))
+                image.paste (feature_image, box=area[0], mask=feature_image)
+                                
+                
     return image
     
 
