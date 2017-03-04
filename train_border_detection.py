b9f5deab321a137b4ee77b1e7db4537428a0db9e
Frank Blankenburg <frankcieslok@gmail.com>
2017-03-04 22:01:27 +0100

    Multilayer convolutional network training added.

--- train_border_detection.py
+++ train_border_detection.py
@@ -57,7 +57,7 @@ class TrainingData:
 #--------------------------------------------------------------------------
 # Train border detection with simple linear model
 #
-def train_simple (data):
+def train_softmax_regression (data):
 
     data_size = data.sample_size * data.sample_size
     all_data = data.get_data ()
@@ -97,7 +97,7 @@ def train_simple (data):
 #--------------------------------------------------------------------------
 # Train border detection with higher level TensorFlow estimator objects
 #
-def train_estimators ():
+def train_estimators (data):
 
     features = [tf.contrib.layers.real_valued_column ("x", dimension=1)]
     
@@ -125,6 +125,83 @@ def train_estimators ():
     estimator.evaluate(input_fn=input_fn)
 
 #--------------------------------------------------------------------------
+# Train border detection with a multilayer convolutional network
+#
+def train_multilayer_convolution (data):
+    
+    def weight_variable (shape):
+      initial = tf.truncated_normal (shape, stddev=0.1)
+      return tf.Variable (initial)
+    
+    def bias_variable (shape):
+      initial = tf.constant (0.1, shape=shape)
+      return tf.Variable (initial)
+      
+    def conv2d (x, W):
+      return tf.nn.conv2d (x, W, strides=[1, 1, 1, 1], padding='SAME')
+    
+    def max_pool_2x2 (x):
+      return tf.nn.max_pool (x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+      
+    data_size = data.sample_size * data.sample_size
+    all_data = data.get_data ()
+
+    print ("Training model...")
+    print ("  Number of samples: ", len (all_data[0]))
+
+    # Create the model
+    x = tf.placeholder (tf.float32, [None, data_size])
+    y_ = tf.placeholder (tf.float32, [None, 2])
+      
+    W_conv1 = weight_variable ([5, 5, 1, 32])
+    b_conv1 = bias_variable ([32])
+    
+    x_image = tf.reshape (x, [-1, data.sample_size, data.sample_size, 1])
+    
+    h_conv1 = tf.nn.relu (conv2d (x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2 (h_conv1)
+    
+    W_conv2 = weight_variable ([5, 5, 32, 64])
+    b_conv2 = bias_variable ([64])
+    
+    h_conv2 = tf.nn.relu (conv2d (h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2 (h_conv2)
+    
+    W_fc1 = weight_variable ([8 * 8 * 64, 1024])
+    b_fc1 = bias_variable ([1024])
+    
+    h_pool2_flat = tf.reshape (h_pool2, [-1, 8 * 8 * 64])
+    h_fc1 = tf.nn.relu (tf.matmul (h_pool2_flat, W_fc1) + b_fc1)
+    
+    keep_prob = tf.placeholder (tf.float32)
+    h_fc1_drop = tf.nn.dropout (h_fc1, keep_prob)
+    
+    W_fc2 = weight_variable ([1024, 2])
+    b_fc2 = bias_variable ([2])
+    
+    y_conv = tf.matmul (h_fc1_drop, W_fc2) + b_fc2
+    
+    cross_entropy = tf.reduce_mean (tf.nn.softmax_cross_entropy_with_logits (labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer (1e-4).minimize (cross_entropy)
+    correct_prediction = tf.equal (tf.argmax (y_conv, 1), tf.argmax (y_, 1))
+    accuracy = tf.reduce_mean (tf.cast (correct_prediction, tf.float32))
+    
+    session = tf.InteractiveSession ()
+    session.run (tf.global_variables_initializer ())
+    
+    for i in range (1000):
+        batch = data.get_next_batch (50)
+        
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval (feed_dict={x:batch[0], y_:batch[1], keep_prob:1.0})
+            print("step %d, training accuracy %g" % (i, train_accuracy))
+    
+        train_step.run (feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+    
+    #print("test accuracy %g" % accuracy.eval (feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+#--------------------------------------------------------------------------
 # MAIN
 #
 
@@ -143,10 +220,11 @@ args = parser.parse_args ()
 training_data = None
 
 file = h5py.File (args.file, 'r')
-training_data = TrainingData (file)
+data = TrainingData (file)
     
-train_simple (training_data)
-#train_estimator (training_data)
+#train_softmax_regression (data)
+#train_estimator (data)
+train_multilayer_convolution (data)
 
 file.close ()
 
