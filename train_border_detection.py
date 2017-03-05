d1662d2e869d8a88ab83df97d0d6b9c3b11e4312
Frank Blankenburg <frankcieslok@gmail.com>
2017-03-05 20:59:29 +0100

    TensorBoard summary generation added.

--- train_border_detection.py
+++ train_border_detection.py
@@ -124,6 +124,7 @@ def train_estimators (data):
     # to use a separate validation and testing data set to avoid overfitting.
     estimator.evaluate(input_fn=input_fn)
 
+
 #--------------------------------------------------------------------------
 # Train border detection with a multilayer convolutional network
 #
@@ -143,6 +144,17 @@ def train_multilayer_convolution (data):
     def max_pool_2x2 (x):
       return tf.nn.max_pool (x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
       
+    def add_variable_summary (var):
+        with tf.name_scope ('summaries'):
+            mean = tf.reduce_mean (var)
+            tf.summary.scalar ('mean', mean)
+            with tf.name_scope ('stddev'):
+                stddev = tf.sqrt (tf.reduce_mean (tf.square (var - mean)))
+            tf.summary.scalar ('stddev', stddev)
+            tf.summary.scalar ('max', tf.reduce_max (var))
+            tf.summary.scalar ('min', tf.reduce_min (var))
+            tf.summary.histogram ('histogram', var)
+      
     data_size = data.sample_size * data.sample_size
     all_data = data.get_data ()
 
@@ -150,22 +162,37 @@ def train_multilayer_convolution (data):
     print ("  Number of samples: ", len (all_data[0]))
 
     # Create the model
-    x = tf.placeholder (tf.float32, [None, data_size])
-    y_ = tf.placeholder (tf.float32, [None, 2])
+    with tf.name_scope ('input'):
+        x = tf.placeholder (tf.float32, [None, data_size])
+        y_ = tf.placeholder (tf.float32, [None, 2])
       
     W_conv1 = weight_variable ([5, 5, 1, 32])
     b_conv1 = bias_variable ([32])
     
-    x_image = tf.reshape (x, [-1, data.sample_size, data.sample_size, 1])
+    with tf.name_scope ('image'):
+        x_image = tf.reshape (x, [-1, data.sample_size, data.sample_size, 1])
+        tf.summary.image ('input', x_image, 10)
     
-    h_conv1 = tf.nn.relu (conv2d (x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2 (h_conv1)
+    with tf.name_scope ('pooling1'):
+        h_conv1 = tf.nn.relu (conv2d (x_image, W_conv1) + b_conv1)
+        h_pool1 = max_pool_2x2 (h_conv1)
+        
+        add_variable_summary (h_conv1)
+        add_variable_summary (h_pool1)
     
-    W_conv2 = weight_variable ([5, 5, 32, 64])
-    b_conv2 = bias_variable ([64])
+    with tf.name_scope ('convolution2'):
+        W_conv2 = weight_variable ([5, 5, 32, 64])
+        b_conv2 = bias_variable ([64])
+        
+        add_variable_summary (W_conv2)
+        add_variable_summary (b_conv2)
     
-    h_conv2 = tf.nn.relu (conv2d (h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2 (h_conv2)
+    with tf.name_scope ('convolution_pooling_2'):
+        h_conv2 = tf.nn.relu (conv2d (h_pool1, W_conv2) + b_conv2)
+        h_pool2 = max_pool_2x2 (h_conv2)
+        
+        add_variable_summary (h_conv2)
+        add_variable_summary (h_pool2)
     
     W_fc1 = weight_variable ([8 * 8 * 64, 1024])
     b_fc1 = bias_variable ([1024])
@@ -173,32 +200,49 @@ def train_multilayer_convolution (data):
     h_pool2_flat = tf.reshape (h_pool2, [-1, 8 * 8 * 64])
     h_fc1 = tf.nn.relu (tf.matmul (h_pool2_flat, W_fc1) + b_fc1)
     
-    keep_prob = tf.placeholder (tf.float32)
-    h_fc1_drop = tf.nn.dropout (h_fc1, keep_prob)
+    with tf.name_scope ('dropout'):
+        keep_prob = tf.placeholder (tf.float32)
+        h_fc1_drop = tf.nn.dropout (h_fc1, keep_prob)
+        
+        tf.summary.scalar ('dropout_keep_probability', keep_prob)
     
     W_fc2 = weight_variable ([1024, 2])
     b_fc2 = bias_variable ([2])
     
-    y_conv = tf.matmul (h_fc1_drop, W_fc2) + b_fc2
+    with tf.name_scope ('y_conv'):
+        y_conv = tf.matmul (h_fc1_drop, W_fc2) + b_fc2
+        
+        add_variable_summary (y_conv)
     
-    cross_entropy = tf.reduce_mean (tf.nn.softmax_cross_entropy_with_logits (labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer (1e-4).minimize (cross_entropy)
-    correct_prediction = tf.equal (tf.argmax (y_conv, 1), tf.argmax (y_, 1))
-    accuracy = tf.reduce_mean (tf.cast (correct_prediction, tf.float32))
+    with tf.name_scope ('cross_entropy'):
+        cross_entropy = tf.reduce_mean (tf.nn.softmax_cross_entropy_with_logits (labels=y_, logits=y_conv))
+        train_step = tf.train.AdamOptimizer (1e-4).minimize (cross_entropy)
+        correct_prediction = tf.equal (tf.argmax (y_conv, 1), tf.argmax (y_, 1))
+        accuracy = tf.reduce_mean (tf.cast (correct_prediction, tf.float32))
+        
+    tf.summary.scalar ('cross_entropy', cross_entropy)
+    tf.summary.scalar ('accuracy', accuracy)
     
     session = tf.InteractiveSession ()
+    
+    merged_summary = tf.summary.merge_all ()
+    train_writer = tf.summary.FileWriter ('log', session.graph) 
     session.run (tf.global_variables_initializer ())
     
-    for i in range (1000):
+    for i in range (300):
         batch = data.get_next_batch (50)
         
         if i % 100 == 0:
             train_accuracy = accuracy.eval (feed_dict={x:batch[0], y_:batch[1], keep_prob:1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
     
-        train_step.run (feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+        summary, _ = session.run ([merged_summary, train_step], feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+        train_writer.add_summary (summary, i)
+        
     
     #print("test accuracy %g" % accuracy.eval (feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    
+    train_writer.close ()
 
 
 #--------------------------------------------------------------------------
@@ -217,8 +261,6 @@ args = parser.parse_args ()
 #
 # Load sample data
 #
-training_data = None
-
 file = h5py.File (args.file, 'r')
 data = TrainingData (file)
     
